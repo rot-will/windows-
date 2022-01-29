70d74422f5b610d86d4b91486f8879b813cee1df
rot.will <69129795+rot-will@users.noreply.github.com>
2022-01-29 21:50:45 +0800

    Update help.py

--- help.py
+++ help.py
@@ -1,9 +1,13 @@
+"""
+@User: rot_will
+@date: 2022.1.29
+"""
 import os
 import argparse
 root_path=r'快捷方式目录'
 ddict={}
 tools_env=root_path+';'
-filelist=[]
+filelist={}
 def updir(cmd,path,dict_path,is_creat=False,hide=True):
     global ddict
     global filelist
@@ -13,7 +17,8 @@ def updir(cmd,path,dict_path,is_creat=False,hide=True):
     for i in cache:
         if os.path.isfile(path+'/'+i):
             exec("%s['%s']=1"%(cmd,i))
-            filelist.append(i)
+            i=i[:i.find('.')]
+            filelist[i]=path
         else:
             if is_creat:
                 if ' ' in path+'\\'+i:
@@ -44,7 +49,7 @@ def getdirlist(path='',is_creat=False,hide=True):
         for i in cache:
             if os.path.isfile(root_path+'\\'+i):
                 ddict['root'][i]=1
-                filelist.append(i)
+                filelist[i]=path
             else:
                 if is_creat:
                     if ' ' in root_path+'\\'+i:
@@ -82,14 +87,12 @@ def showdict(dir_dict,pad="",search_str="",is_show_file=True,is_dire=False,dire_
         print("%s-- ..."%(pad))
     return n
 
-def addbat(cmd,name,path='',real_dir='',is_start=True):
-    if name+'.bat' in filelist:
-        exit("There are duplicate options")
+def editbat(name,cmd,path,real_dir,is_start):
     bat_file=open(root_path+'/'+path+'/'+name+'.bat','wb')
     direct="""@echo off
 set sour_dir=%CD%
 set dl=%CD:~0,2%
-{} %*
+{} %* 
 %dl%
 cd %sour_dir%
 @echo on
@@ -98,17 +101,36 @@ cd %sour_dir%
     if real_dir:
         cd_dir+=real_dir[:2]+'\n'
         cd_dir+="cd "+real_dir+'\n'
+    cmd=cmd.replace("'",'"')
     if os.path.isfile(cmd):
         if '.exe' in cmd:
+            #direct=direct.format(cd_dir+'start '*is_start+'%s'%cmd)
             if ' ' in cmd:
-                direct=direct.format(cd_dir+'start '*is_start+'"%s"'%cmd)
+                direct=direct.format(cd_dir+'start "" '*is_start+'"%s"'%cmd)
             else:
-                direct=direct.format(cd_dir+'start '*is_start+'%s'%cmd)
+                direct=direct.format(cd_dir+'start "" '*is_start+'%s'%cmd)
         else:
-            direct=direct.format(cd_dir+'"%s"'%cmd.replace("'",'"'))
+            direct=direct.format(cd_dir+'"%s"'%cmd)
     else:
-        direct=direct.format(cd_dir+(cmd.replace("'",'"')))
+        direct=direct.format(cd_dir+cmd)
     bat_file.write(direct.encode())
+    
+def addbat(name,cmd='',path='',real_dir='',is_start=True,is_re=False):
+    if is_re:
+        if path and cmd:
+            exit("Only command contents or command directories can be replaced")
+        elif path:
+            os.rename(filelist[name]+'\\'+name+'.bat',root_path+'\\'+path+'\\'+name+'.bat')
+            return 0
+        elif cmd:
+            editbat(name,cmd,filelist[name][len(root_path):],real_dir,is_start)
+            return 0
+        else:
+            exit("When replace exists, a directory or command must exist")
+    elif name in filelist:
+        exit("There are duplicate options")
+    editbat(name,cmd,path,real_dir,is_start)
+    
 def deldir(path):
     cache=os.listdir(path)
     for i in cache:
@@ -141,18 +163,32 @@ def create(path):
     setenv()
     
 def help(parse):
+    """ show """
     parse.add_argument('-c','--create',dest='is_creat',action='store_true', default=False,help="Construct system variables default:False")
     parse.add_argument('-help',dest='show_type',action='store_true', default=False,help="view type default:False")
     parse.add_argument('-hide',dest='is_hide',action='store_false', default=True,help="Show hide commands default:True")
+    
+    """ search """
     parse.add_argument('-s','--search',dest='search_str',default='',help="Search specified string")
-    parse.add_argument('-dire',dest='dire_str',action='store_true',default=False,help='Search specified directory')
+    parse.add_argument('-dire',dest='dire_str',action='store_true',default=False,help='Search specified type')
+    
+    """ command_data """
     parse.add_argument('-d','--direct',dest='direct',default='',help='Specify command')
     parse.add_argument('-start',dest='is_start',action='store_false', default=True,help="Do you want to use start to launch exe")
-    parse.add_argument('-n','--name',dest='name',default='',help='Specify script name')
-    parse.add_argument('-t','--type',dest='type',default='',help='Type of command xx/xxx/xxxx')
     parse.add_argument('-tardir',dest='target_dir',default='',help="Specify target program directory")
-    parse.add_argument('-add',dest='add_dire',help='Added directory')
-    parse.add_argument('-del',dest='del_dire',help='Delete command or directory')
+    
+    """ command """
+    parse.add_argument('-n','--name',dest='name',default='',help='Specify script name')
+    parse.add_argument('-replace',dest='is_re',action='store_true',default=False,help="Replace the original command default:False")
+    
+    """ type """
+    parse.add_argument('-t','--type',dest='type',default='',help='Type of command xx/xxx/xxxx')
+    
+    """ create type """
+    parse.add_argument('-add',dest='add_dire',help='Added type')
+    
+    """ delete type/command """
+    parse.add_argument('-del',dest='del_dire',help='Delete command or type')
     
 
 def main():
@@ -167,10 +203,10 @@ def main():
         print("\n--------------- view Type ---------------\n")
         showdict(ddict,'',is_show_file=not args.show_type)
         exit(0)
-    elif args.direct:
-        if not (args.name or args.type):
-            exit("When direct exists, name is required")
-        addbat(args.direct,args.name,args.type,args.target_dir,args.is_start)
+    elif args.name:
+        if not (((args.direct or args.type) and args.is_re) or ((args.direct and args.type) or args.is_re)):
+            exit("When name exists, direct is required")
+        addbat(args.name,args.direct,args.type,args.target_dir,args.is_start,args.is_re)
     elif args.del_dire:
         delete(args.del_dire)
     elif args.add_dire:
